d3f1568588762156cb30d2cdbcf1e8cd8efeea82
sumanbogati <sumanbogati@gmail.com>
2015-05-04 13:00:14 +0530

    added file

---
+++ src/progressbar.js
@@ -0,0 +1,44 @@
+// This file is part of Vidyamantra - http:www.vidyamantra.com/
+/**@Copyright 2015  Vidyamantra Edusystems. Pvt.Ltd.
+ * @author  Suman Bogati <http://www.vidyamantra.com>
+  */
+
+var progressBar = {
+    prvVal : '',
+    currVal : '',
+    progressInit : function (){
+        var that = this;
+        if(typeof prvTimeout != 'undefined'){
+            clearTimeout(prvTimeout);
+        }
+        
+        prvTimeout = setTimeout(
+            function (){
+                if(that.prvVal == that.currVal){
+                    console.log("start for download");
+                    vApp.recorder.tryForReTransmit();
+                } else {
+                    that.prvVal = that.currVal;
+                    that.progressInit();
+                }
+            }, 50000
+        );
+    },
+    
+     renderProgressBar : function (totalVal, portion, pbar, pval){
+        if(portion > totalVal){
+            portion = totalVal;
+            document.getElementById('askplayMessage').innerHTML =  vApp.lang.getString('playsessionmsg');
+        }
+
+        if(totalVal == 0 && portion == 0){
+            var totalProgress =  0;
+        }else {
+            var totalProgress=Math.round((portion*100)/totalVal);
+        }
+
+        document.getElementById(pbar).style.width=totalProgress+'%';
+        document.getElementById(pval).innerHTML=totalProgress+'%';
+
+    }
+}

f228232afc266c654d2ffb22014566d6156264f0
sumanbogati <sumanbogati@gmail.com>
2015-10-19 17:38:59 +0530

    update language file

--- example/en.php
+++ example/en.php
@@ -3,15 +3,15 @@
     $string['uploadsession'] = 'Please wait until processing is complete.';
     $string['totalprogress'] = 'Total Progress';
     $string['indvprogress'] = 'Current Task';
-    $string['downloadsession'] = 'Please wait while recording is downloaded.';
-    $string['askplaymsg'] = '<span id="askplaymsg"> "Downloading under process, click Play  to begin </span>"';
+    $string['downloadsession'] = 'Please wait while the recording is downloaded.';
+    $string['askplaymsg'] = '<span id="askplaymsg"> "Downloading in process, click Play to begin </span>"';
     $string['overallprogress'] = 'Overall Progress';
     $string['replay_message'] = 'Thanks for watching.';
     $string['uploadedsession'] = 'Recording saved successfully';
     $string['sessionendmsg'] = 'Session has been closed. You may now close your browser.';
     $string['play'] = 'Play';
     $string['replay'] = 'Re-play';
-    $string['tpAudioTest'] = 'Audio Test';
+    $string['tpAudioTest'] = ‘Test Audio';
     $string['enableAudio'] = 'Unmute';
     $string['disableAudio'] = 'Mute';
     $string['pressAlwaysToSpeak'] = 'Press always to speak.';

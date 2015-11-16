324b3e9ece9a52bf84730773dc2319162cca87c7
sumanbogati <sumanbogati@gmail.com>
2015-11-16 15:15:00 +0530

    update js.debug.php file in build folder

--- build/js.debug.php
+++ build/js.debug.php
@@ -1,47 +1,100 @@
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>bundle/jquery/jquery-ui.min.js"></script>
 
 
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/vmapp.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/vcan.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whboard.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/wb_utility.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/en.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>bundle/io/src/iolib.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/roles.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/io-storage.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/io-missing-packets.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/io-adapter.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/io-ping-pong.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/virtualclass.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-canvas.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-utility.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/lang-en.js"></script>
 <script type="text/javascript" src="<?php echo $whiteboardpath;?>src/lang.js"></script>
 <script type="text/javascript" src="<?php echo $whiteboardpath;?>src/view.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/system.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/packetcontainer.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/drawobject.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/makeobj.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/utility.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/vcanmain.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/events.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/virtualbox.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/interact.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/rectangle.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/oval.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/triangle.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/line.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/text.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/freedrawing.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/path.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/mouse.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/readyfreehandobj.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/replay.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/readytextobj.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/keyboard.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/adapter.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/resampler.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/environment-validation.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-packetcontainer.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-drawobject.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-make-object.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-canvas-utility.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-canvas-main.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-events.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-virtualbox.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-interact.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-rectangle.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-oval.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-triangle.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-line.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-text.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-freedrawing.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-path.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-mouse.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-readyfreehandobj.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-refresh-play.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-readytextobj.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-keyboard.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/webrtc-adapter.js"></script>
+<!--<script type="text/javascript" src="<?php //echo $whiteboardpath;?>src/com.js"></script>-->
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/audio-resampler.js"></script>
 <script type="text/javascript" src="<?php echo $whiteboardpath;?>src/media.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/bridge.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/optimization.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/response.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/g711.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/getscreen.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/dirtycorner.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/vutility.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-packet-queue.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/whiteboard-optimization.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/receive-messages-response.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/lzstring.js"></script>
+
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/audio-codec-g711.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/screenshare-getscreen.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/screenshare-dirtycorner.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/utility.js"></script>
 <script type="text/javascript" src="<?php echo $whiteboardpath;?>src/screenshare.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/recorder.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/storage.js"></script>
-<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/user.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/record-play.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/indexeddb-storage.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/footer-control-user.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/xhr.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/popup.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/storage-array-base64-converter.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/progressbar.js"></script>
+
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/youtube-iframe-api.js"></script>
+
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/youtube.js"></script>
+
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>codemirror/lib/codemirror.js"></script>
+
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>codemirror/addon/edit/continuelist.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>codemirror/mode/xml/xml.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>codemirror/mode/markdown/markdown.js"></script>
+
+
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/ot-server.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/editor-utils.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/editor-rich-toolbar.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/editor-text-op.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/ot-text-operation.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/ot-wrapped-operation.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/ot-cursor.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/ot-undo-manager.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/ot-client.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/ot-editor-client.js"></script>
+
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/editor-span.js"></script>
+<script type="text/javascript" src="<?php  echo $whiteboardpath;?>src/editor-annotation-list.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/editor-attribute-constants.js"></script>
+
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/editor-line-formatting.js"></script>
+
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/editor-serialize-html.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/editor-parse-html.js"></script>
+
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/ot-codemirror-adapter.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/ot-adapter.js"></script>
+
+
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/vceditor.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>src/editor.js"></script>
 <script type="text/javascript" src="<?php echo $whiteboardpath;?>chat/chat.js"></script>
 <script type="text/javascript" src="<?php echo $whiteboardpath;?>chat/footer.js"></script>
 <script type="text/javascript" src="<?php echo $whiteboardpath;?>chat/jquery.ui.chatlist.js"></script>
@@ -50,6 +103,5 @@
 <script type="text/javascript" src="<?php echo $whiteboardpath;?>chat/chatboxManager.js"></script>
 <script type="text/javascript" src="<?php echo $whiteboardpath;?>chat/lib.js"></script>
 <script type="text/javascript" src="<?php echo $whiteboardpath;?>chat/lang.en.js"></script>
+<script type="text/javascript" src="<?php echo $whiteboardpath;?>index.js"></script>
 
-
-      

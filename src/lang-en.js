aba2913a306369a4580f98e6e77c14ca4efa90b8
nirmala10 <nirmala@vidyamantra.com>
2018-05-18 12:25:15 +0530

    document reload to normal render

--- src/lang-en.js
+++ src/lang-en.js
@@ -432,7 +432,8 @@
         'Replay': "Replay from start.",
         'Finish': "Finish",
         'oncollaboration': "Collaboration on",
-        'offcollaboration': "Collaboration off"
+        'offcollaboration': "Collaboration off",
+        'reloadDoc':"Reload doument",
 
     };
     window.message = message;

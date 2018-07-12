31ac06c59552688227972265e92e92f88ae907f2
Shubham Vaishala <shubham.vaishala@vidyamantra.com>
2018-07-12 09:44:18 +0530

    replace key

--- example/auth.php
+++ example/auth.php
@@ -30,8 +30,8 @@ $secret = '8ww4umRbFIcTu92cJhrExXWNNAG7FDuN91RDoJO3s1XWa09UiBzPyvDl6ksLXXz6'; */
 $secret = 'iS3pXEWVgclNdEd2QezeJpwJDyGYn8LF0O5Ys1UJefxrUKtKZ3YJsvs6494rhP6i'; */
 
 
-$licensekey = 'i8d1T08fAcvAZGTvJmj8hIFWQY5jSY851CTUn7DoyCkjJnvD';
-$secret = 'VsrArG07d6oKqINC4dnYEhn6lhDm3BNY3ezvVzC9G0T7JGKNT7Myo9EKri7uTUtX';
+$licensekey = 'U8ZhqgxOuaQrmOfdtDVBpcKO0ZpuNdM85CYl6Mb41CLGnY3F';
+$secret = 'IABs5pZVa98OHuhYvXe6wh1CjB0YKIjsjVUolTkv3tjLXBKtyYA5fBIydJwXrQqK';
 
 
 /*

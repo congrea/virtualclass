a3f1a390c39aafda3b1789f5ce36767599fbf47f
nirmala <nirmala@vidyamantra.com>
2018-09-26 15:29:51 +0530

    whiteboard student pagination

--- src/whiteboard-utility.js
+++ src/whiteboard-utility.js
@@ -686,6 +686,8 @@
                 }
 
                 window.virtualclass.wb[id].init(id);
+               
+             
 
                 virtualclass.wb[id].utility.makeCanvasDisable();
                 virtualclass.wb[id].utility.toolWrapperDisable();
@@ -695,6 +697,12 @@
                    //     virtualclass.wb[id].utility.createWhiteboardMessage()
                     }
                 }
+                if (!roles.hasControls()) {
+                    if (typeof virtualclass.wbCommon.indexNav !== 'undefined') {
+                        virtualclass.wbCommon.indexNav.studentWBPagination(virtualclass.gObj.currSlide);
+                    }
+                }
+                
             },
 
             removeWhiteboardMessage : function (){

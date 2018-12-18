3087d82b6447dd07feff5fd2e29987cde79ea8a9
nirmala <nirmala@vidyamantra.com>
2018-12-18 15:54:21 +0530

    poll , quiz bootstrap removal bug fixed

--- src/poll.js
+++ src/poll.js
@@ -1049,6 +1049,10 @@
                 return 1;
             },
             saveNdPublish: function (index, type) {
+                var flag = virtualclass.poll.isBlank();
+                if (!flag) {
+                    return 0;
+                }
                 var pollType = type + "Poll";
                 var length = virtualclass.poll[pollType].length
                 var optsCont = document.getElementById('optsTxCont');

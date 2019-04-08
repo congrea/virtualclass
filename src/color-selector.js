ebb511125eb59e7c0af3bd452804fb25abbe1c73
Rohitsharma2019 <rohit.sharma@vidyamantra.com>
2019-04-08 15:47:59 +0530

    some ui changes from my side

--- src/color-selector.js
+++ src/color-selector.js
@@ -83,9 +83,9 @@ var colorSelector ={
             "{border-left: 0.01em solid "+allbg.fcolor +"!important;}"+
             // "#virtualclassCont.congrea #virtualclassOptionsCont #virtualclassWhiteboardTool" +
             // "{border:0 !important;}"+
-            "#virtualclassCont.congrea .zoomControler div, #virtualclassCont.congrea #networkStatusContainer, #virtualclassCont.congrea #audioWidget li" +
+            "#virtualclassCont.congrea #audioWidget li" +
             "{border-right:"+border +" !important;}"+
-            "#virtualclassCont.congrea .zoomControler,#virtualclassCont.congrea #networkStatusContainer, " +
+            "#virtualclassCont.congrea .zoomControler div, #virtualclassCont.congrea #virtualclassAppRightPanel #chatWidget .chatBarTab, #virtualclassCont.congrea #audioWidget .audioTool, #virtualclassCont.congrea #networkStatusContainer, " +
             "#virtualclassCont.congrea #alleditorRichContainerAnch" +
             "{border-left:"+border +" !important;}"+
             "#virtualclassCont.congrea .containerWb .commandToolsWrapper .tool a ," +

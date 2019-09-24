6bc62ce0d55aed04792181788de0ec3f80bc2b73
uditbh <udit.bhargav@gmail.com>
2019-09-24 11:10:33 +0530

    update no of online users out of total users String for moodle

--- src/chat-lib.js
+++ src/chat-lib.js
@@ -129,14 +129,14 @@ function displayChatOfflineUserList(users) {
 function updateOnlineUserText() {
   if (roles.hasAdmin()) {
     if (virtualclass.chat.userList.length > 0) {
-      document.querySelector('#usertab_text').innerHTML = '';
+      document.querySelector('#usertab_text #onlineusertext').innerHTML = '';
       if (roles.hasAdmin()) {
         var text = `Users (${virtualclass.connectedUsers.length}/${virtualclass.chat.userList.length})`;
       } else {
         var text = ` Users (${count})`;
       }
 
-      const onlineUser = document.querySelector('#onlineusertext');
+      const onlineUser = document.querySelector('#usertab_text #onlineusertext');
 
       if (onlineUser == null) {
         document.querySelector('#usertab_text').innerHTML = `<span id='onlineusertext' class='cgText'>${text}</span>`;

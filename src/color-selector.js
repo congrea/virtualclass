1669c6ed3a466f5c2cbd2d927e840d4e60ba16c6
uditbh <udit.bhargav@gmail.com>
2019-10-17 14:27:54 +0530

    Page navigation styling

--- src/color-selector.js
+++ src/color-selector.js
@@ -178,16 +178,7 @@
         + '#virtualclassCont.congrea .cgIcon:before'
         + `{color:${frontColor}!important}`
 
-        + '#virtualclassCont.congrea #docShareNav #totalPages'
-        + `{color:${frontColor}!important}`
-
-      //            "#virtualclassCont.congrea #dcPaging .noteIndex"+
-      //            "{color:"+frontColor+"!important}"+
-
         + '#virtualclassCont.congrea #docShareNav .pageNav'
-        + `{color:${frontColor}!important}`
-
-        + '#virtualclassCont.congrea #docShareNav #dcPaging #stdPageNo'
         + `{color:${frontColor}!important}`
 
         + '#virtualclassCont.congrea .containerWb .commandToolsWrapper .tool a'

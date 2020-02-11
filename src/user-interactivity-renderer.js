accb5fee35ceb1abe7150367b151991528d33f2a
suman <sumanbogati@gmail.com>
2020-02-11 15:01:34 +0530

    Fix bug #1332

--- src/user-interactivity-renderer.js
+++ src/user-interactivity-renderer.js
@@ -118,7 +118,7 @@ class UserInteractivityRenderer { // Main Part
     if (data.component === 'question') {
       const chkContextElem = document.querySelector(`#askQuestion .context[data-context~=${data.context}]`);
       if ('question' && chkContextElem) {
-        const componentTemplate = virtualclass.getTemplate(data.component, 'askQuestion');
+        const componentTemplate = document.getTemplate(data.component, 'askQuestion');
         const htmlContent = componentTemplate({ id: data.id, userName: data.uname, content: text.content, morecontent: text.moreContent });
         document.querySelector(`#askQuestion [data-context~=${data.context}] .container`).insertAdjacentHTML('beforeend', htmlContent);
       } else {

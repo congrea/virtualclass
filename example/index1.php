bbba65ebf8f39789e5a5795b493143cb26edb86c
uditbh <udit.bhargav@gmail.com>
2020-03-18 15:49:56 +0530

    Traditional meta tags for IOS devices

--- example/index1.php
+++ example/index1.php
@@ -13,7 +13,20 @@
 <meta name="apple-mobile-web-app-status-bar-style" content="black">
 <meta name="apple-mobile-web-app-title" content="Congrea">
 <link rel="apple-touch-icon" href="https://cdn.glitch.com/49d34dc6-8fbd-46bb-8221-b99ffd36f1af%2Fapple-touch-icon.png?v=1566861131254">
-<!-- <link rel="apple-touch-icon" href="https://live.congrea.net/virtualclass/images/maskable_icon.png" sizes="180x180"> -->
+<!-- iPhone X (1125px x 2436px) SPLASH IMAGE -->
+<link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" href="https://live.congrea.net/virtualclass/images/launch-750x1294.png">
+<!-- iPhone 8, 7, 6s, 6 (750px x 1334px) SPLASH IMAGE -->
+<link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" href="https://live.congrea.net/virtualclass/images/launch-750x1294.png">
+<!-- iPhone 8 Plus, 7 Plus, 6s Plus, 6 Plus (1242px x 2208px) SPLASH IMAGE -->
+<link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)" href="https://live.congrea.net/virtualclass/images/launch-750x1294.png">
+<!-- iPhone 5 (640px x 1136px) SPLASH IMAGE -->
+<link rel="apple-touch-startup-image" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" href="https://live.congrea.net/virtualclass/images/launch-750x1294.png">
+<!-- iPad Mini, Air (1536px x 2048px) SPLASH IMAGE -->
+<link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" href="https://live.congrea.net/virtualclass/images/launch-750x1294.png">
+<!-- iPad Pro 10.5" (1668px x 2224px) SPLASH IMAGE -->
+<link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" href="https://live.congrea.net/virtualclass/images/launch-750x1294.png">
+<!-- iPad Pro 12.9" (2048px x 2732px) SPLASH IMAGE -->
+<link rel="apple-touch-startup-image" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" href="https://live.congrea.net/virtualclass/images/launch-750x1294.png">
 <!-- CODELAB: Add description here -->
 <meta name="description" content="Congrea">
 <!-- CODELAB: Add meta theme-color -->

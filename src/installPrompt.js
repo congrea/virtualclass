bb1e475cec6052c187358501b004816a2fbcbb51
uditbh <udit.bhargav@gmail.com>
2020-03-31 11:50:46 +0530

    Track how pwa launch

--- src/installPrompt.js
+++ src/installPrompt.js
@@ -3,9 +3,18 @@ window.addEventListener('beforeinstallprompt', (event) => {
   // Prevent the mini-infobar from appearing on mobile
   event.preventDefault();
   // Stash the event so it can be triggered later.
-  //deferredPrompt = event;
   window.deferredPrompt = event;
-  // Update UI notify the user they can install the PWA
-  // showInstallPromotion();
-  // divInstall.classList.toggle('hidden', false);
+});
+
+// Track how the PWA was launched 
+window.addEventListener('load', () => {
+  if (navigator.fullscreen) {
+    console.log('Launched: Installed (iOS)');
+    document.querySelector("html").classList.add("pwa");
+  } else if (matchMedia('(display-mode: fullscreen)').matches) {
+    console.log('Launched: Installed');
+    document.querySelector("html").classList.add("pwa");
+  } else {
+    console.log('Launched: Browser Tab');
+  }
 });

0e13b576ba8f4f952cf3191ee1d597da447349a2
suman <sumanbogati@gmail.com>
2020-04-01 11:08:52 +0530

    Add keyboard file

---
+++ src/keyboard.js
@@ -0,0 +1,22 @@
+class VirtualclassKeyboard {
+  constructor() {
+  }
+
+  init(wId) {
+    // const canvas = document.querySelector(`#canvasWrapper${wId} .upper-canvas`);
+    // if(canvas) canvas.addEventListener('keydown', this.handlerKeyDown);
+
+    var canvasWrapper = document.querySelector(`#canvasWrapper${wId}`);
+    canvasWrapper.tabIndex = 1000;
+    canvasWrapper.addEventListener("keydown", this.handlerKeyDown, false);
+  }
+
+  handlerKeyDown(evt){
+    // This is used for removed the selected object.
+    // var currTime = new Date().getTime();
+    // 8 is used for delete on mac
+    if (evt.keyCode === 8 || evt.keyCode === 46) {
+      virtualclass.wbWrapper.util.deleteActiveObject(event);
+    } 
+  }
+}

e662b5bc9a892f4080918f6bdfc03ca304774756
uditbh <udit.bhargava@vidyamantra.com>
2020-06-18 18:16:55 +0530

    Add PWA support for new congrea plugin only

--- src/installPrompt.js
+++ src/installPrompt.js
@@ -1,10 +1,14 @@
-window.addEventListener('beforeinstallprompt', (event) => {
-  console.log('👍', 'beforeinstallprompt', event);
-  // Prevent the mini-infobar from appearing on mobile
-  event.preventDefault();
-  // Stash the event so it can be triggered later.
-  window.deferredPrompt = event;
-});
+if(window.location.search !== "null" && window.system.device=="mobTab" && window.system.mybrowser.name == "Chrome") {
+  // when plugin has a support of pwa
+  document.querySelector("html").classList.add("pwaSupported");
+  window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('👍', 'beforeinstallprompt', event);
+    // Prevent the mini-infobar from appearing on mobile
+    event.preventDefault();
+    // Stash the event so it can be triggered later.
+    window.deferredPrompt = event;
+  });
+}
 
 // Track how the PWA was launched 
 window.addEventListener('load', () => {
